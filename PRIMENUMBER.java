f926cc8f188de7b0d459e7c9c86f80cc8868778d
patelparij2302-tech <patelparij2302@gmail.com>
2026-07-23 12:38:28 +0530

    Add files via upload

---
+++ PRIMENUMBER.java
@@ -0,0 +1,18 @@
+class PRIMENUMBER {
+    public static void main(String[] args) {
+        int num = 10;
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            System.out.println(num + " is a Prime Number");
+        } else {
+            System.out.println(num + " is Not a Prime Number");
+        }
+    }
+}
