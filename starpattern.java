b6942510795cb1313022da5cacdbca54bd5ec4a1
patelparij2302-tech <patelparij2302@gmail.com>
2026-07-30 11:43:37 +0530

    Add files via upload

---
+++ starpattern.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+ class starpattern {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = 3;
+
+        System.out.print("Enter a symbol: ");
+        String star = s.next();
+
+        
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(star);
+            }
+
+            System.out.println();
+        }
+
+        
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(star);
+            }
+
+            System.out.println();
+        }
+
+        s.close();
+    }
+}
