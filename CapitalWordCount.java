71e1e7f26463edf3a3c0ff6f5aa9824b3ae8911b
patelparij2302-tech <patelparij2302@gmail.com>
2026-08-04 22:09:23 +0530

    Add files via upload

---
+++ CapitalWordCount.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class CapitalWordCount
+{
+
+public static void main(String[] args)
+{
+int i;
+Scanner ob1 = new Scanner(System.in);
+System.out.println("enter a line:");
+String str = ob1.nextLine();
+
+String[] word = str.split(" ");
+int count = 0;
+
+for(i=0; i < word.length ; i++)
+{
+char ch = word[i].charAt(0);
+
+if(ch >= 'A' && ch<='Z')
+{
+count ++;
+}
+}
+
+System.out.println("number of word s starting with capital letter:" + count);
+}
+}
